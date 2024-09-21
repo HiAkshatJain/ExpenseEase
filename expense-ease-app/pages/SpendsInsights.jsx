0fea887480748a93fd4ad7fb9f197b8f020419b9
Akshat Jain <realakshatjain@gmail.com>
2024-09-22 02:54:04 +0530

    ui pard done

---
+++ expense-ease-app/pages/SpendsInsights.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import CustomText from "../component/CustomText";
+
+const SpendsInsights = () => {
+  const insightsData = [
+    { id: "1", label: "Status", value: "At Risk" },
+    { id: "2", label: "Amount limit", value: "10,000 Rs." },
+    { id: "3", label: "Most Spend Category", value: "Shopping" },
+  ];
+
+  return (
+    <View style={styles.spendingStatusContainer}>
+      {insightsData.map((insight) => (
+        <View key={insight.id}>
+          <CustomText style={{}}>
+            {insight.id}. {insight.label}: {insight.value}
+          </CustomText>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default SpendsInsights;
+
+const styles = StyleSheet.create({
+  spendingStatusContainer: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginLeft: 10,
+  },
+});
